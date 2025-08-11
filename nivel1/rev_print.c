372bb2834c71ff9cfdc08e36e1e22729b983d783
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-08-11 20:06:09 +0200

    examen

---
+++ nivel1/rev_print.c
@@ -0,0 +1,36 @@
+/*Escribe una función que reciba una cadena de caracteres y la muestre en orden inverso, seguida de un salto de línea.
+El prototipo de la función debe ser:
+
+char *ft_rev_print(char *str);
+
+La función debe devolver el mismo puntero recibido como argumento.
+Solo se permite usar la función write (no se puede usar printf, puts, etc.).*/
+
+#include <unistd.h>
+
+char *ft_rev_print(char *str)
+{
+    int i = 0;
+
+    while(str[i])
+        i++;
+    i--;
+    while(i >= 0)
+    {
+        write(1, &str[i], 1);
+        i--;
+
+    }
+    write(1, "\n", 1);
+    return(str);
+}
+
+/*int main()
+{
+    char str[] = "piloto";
+
+    ft_rev_print(str);
+
+    return (0);
+
+}*/
