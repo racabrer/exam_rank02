fc4b11afff24c2ce99edfcc27cb2c5afd2771429
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-02 19:56:06 +0200

    repeat_alpga

--- nivel1/repeat_alpha.c
+++ nivel1/repeat_alpha.c
@@ -67,3 +67,26 @@ int main (int argc, char **argv)
     write(1, "\n", 1);
     return (0);
 }
+
+/*
+    Esta función repite cada carácter según el número que le corresponde en el orden del abecedario 
+    "a" -> a, "b" -> bb, "c" -> ccc y así sucesivamente.
+
+    Declaramos dos variables, una para iterar los argumentos 
+    y otra para calcular cuántas veces tenemos que repetir el carácter
+    Comprobamos el número de argumentos
+    Recorremos argv 
+        Hacemos dos bucles if, uno para las mayúsculas y otro para las minúsculas.
+        El proceso es el mismo para ambos, solo cambian los números ascii
+        mayúsculas -> 65 - 90 minúsculas -> 97 - 122
+        Si argv está entre a (o A) y z (o Z)
+            Reinicia j a 0 ** ESTE PASO ES ESENCIAL **
+            Mientras que j sea menor que argv[1][i] - 64 (o 89)
+                write(1, &argv[1][i], 1)
+                Avanza en j (j++)
+        Sino
+            Escribe argv
+        i++
+    \n
+    Fin del programa
+*/
