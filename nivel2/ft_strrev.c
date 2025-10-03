301fe4164a41df91f39b7fbce05b6ea3d154a70d
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 21:04:39 +0200

    ft_strrev

--- nivel2/ft_strrev.c
+++ nivel2/ft_strrev.c
@@ -44,3 +44,25 @@ int	main(void)
 	printf("%s\n", s);
 	return (0);
 }
+
+/*
+Invierte un string en el lugar (modifica directamente la cadena original)
+-Declaramos tres variables:
+    i-> iterador
+    len->calcula el tamaño del string
+    temp-> variable temporal para hacer el intercambio de caracteres
+-Inicializamos i y len a cero
+
+PROCESO DE INVERTIR LA CADENA
+-Mientras que i sea menor que len -1 (-1 es por ‘\0’) . 
+Lo que vamos a hacer es intercambiar los caracteres desde los extremos hacia el centro.
+(Str[i] es el carácter desde el que vamos a comenzar
+str[len - 1] es el carácter desde el final)
+-guardamos temporalmente el carácter ubicado en la posición i de la cadena str dentro de la variable temp
+-copiamos el carácter del final de la cadena en la posición de inicio
+-completamos el intercambio entre los dos caracteres. Usamos temp para 
+    poner el carácter original de str[i] en la posición final.
+i++
+len--
+Devuelve str que ya está invertida -> return (str)
+*/
