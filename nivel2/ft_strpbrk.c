eed767073f2ab2ca347d45d018b603b768faccc3
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 21:02:30 +0200

    ft_strpbrk

--- nivel2/ft_strpbrk.c
+++ nivel2/ft_strpbrk.c
@@ -51,3 +51,17 @@ char	*ft_strpbrk(const char *s1, const char *s2)
      }
      return(NULL);
 }
+
+/*
+Esta fusión busca en una cadena s1 el primer carácter que también esté presente en otra cadena s2,
+si lo encuentra, devuelve un puntero a ese carácter dentro de s, si no lo encuentra, devuelve NULL
+Declaramos dos variables que son iteradores i y j
+Mientras s1[i]
+     Mientras s2[j]
+          Si s1[i] y s2[j] son iguales
+               Devuelve ((char *) &s1[i])
+               ( -> puntero al carácter, importante hacer el casteo )
+          Avanzamos en j (j++)
+     Avanzamos en i (i++)
+Sino devuelve NULL, en este caso será que no hemos encontrado coincidencia.
+*/
