448eb59415d167dc570b7d877cae6e1f10cc70ce
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 20:58:11 +0200

    strcmp

--- nivel2/ft_strcmp.c
+++ nivel2/ft_strcmp.c
@@ -20,3 +20,11 @@ int    ft_strcmp(char *s1, char *s2)
         i++;
     return (s1[i] - s2[i]);
 }
+
+/*
+Esta función compara s1 y s2
+Para ello crea inicializa una variable i que va a utilizar como iterador para recorrer ambas cadenas
+Mientras que exista s1 o s2 y que s1 sea igual a s2
+    Avanzamos en i
+Devolvemos la diferencia entre s1 y s2
+*/
