c29d656b3140682c3d79684faf78773301e67420
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 20:59:44 +0200

    strdup

--- nivel2/ft_strdup.c
+++ nivel2/ft_strdup.c
@@ -32,3 +32,18 @@ char    *ft_strdup(char *src)
     tmp[i] = 0;
     return(tmp);
 }
+
+/*
+Esta función duplica un string y devuelve un puntero a la nueva cadena.
+Declaramos tres variables:
+    Char *temp -> donde guardaremos la cadena
+    Int len -> tamaño de la cadena
+    Int i -> iterador
+Recorremos la cadena hasta el final con len -> len++ -> contendrá el tamaño del string
+Asignamos espacio con malloc al string temporal temp y hacemos la comprobación de malloc
+Mientras que exista src
+    Copiamos el valor de src en la variable temporal tmp = src
+    i++
+Añadimos un cero al final a temp
+Devuelve temp. Que es la copia del string.
+*/
