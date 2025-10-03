9b541961c83c5c86b5497f6d20d8ed358a225b07
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 21:11:13 +0200

    is_power_of_2

--- nivel2/is_power_of_2.c
+++ nivel2/is_power_of_2.c
@@ -24,3 +24,11 @@ int	    is_power_of_2(unsigned int n)
     else 
         return (0);
 }
+
+/*
+Esta función devuelve uno si n es potencia de dos y devuelve cero si no es potencia de dos
+Si n es igual a cero, devuelve cero porque cero no es potencia de dos
+Mientras que el módulo de n entre dos sea cero(mientras que n sea divisible por 2),
+    Dividimos n entre dos -> verificamos si n sigue siendo divisible por dos.
+Si tras hacer esto n es 1, devuelve 1 porque significará que es potencia de dos, en caso contratio, devuelve cero
+*/
