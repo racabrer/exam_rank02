73dd0f0112e5d65ae94052699497677be40f344c
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 21:10:10 +0200

    inter

--- nivel2/inter.c
+++ nivel2/inter.c
@@ -56,3 +56,23 @@ int main(int argc, char **argv)
     write(1, "\n", 1);
     return (0);
 }
+
+/*
+Este programa, recibe dos strings y sin que se repitan los caracteres,
+escribe las coincidencias, tienen que mantener el orden del primer string para que sea válido
+Declaramos tres variables:
+    i-> es un iterador para recorrer la primera cadena
+    j-> otro iterador que recorre la segunda cadena
+    lookup[256] = {} -> esto representa una tabla que contiene todos los valores de ascii
+     y va marcando los caracteres que ya han coincidido para que no se repitan
+-Comprobamos el número de argumentos
+    -Recorremos la primera cadena
+        -Recorremos la segunda
+            -Mientras que ambas sean iguales y  lookup no está activado
+                - Igualamos a 1 la posición de lookup porque hemos encontrado una coincidencia
+                - write(1, &argv[1][i], 1);
+            j++
+        i++;
+\n
+Return 0
+*/
