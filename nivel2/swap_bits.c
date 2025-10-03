8adf0e33a90140210036eae5bf7fe7a3ca893e4f
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 21:14:02 +0200

    swap_bits

--- nivel2/swap_bits.c
+++ nivel2/swap_bits.c
@@ -27,3 +27,11 @@ unsigned char	swap_bits(unsigned char octet)
     return ((octet >> 4 | octet << 4));
 }
 
+/*
+Esta función intercambia los 4 bits superiores con los 4 bits inferiores de un byte.
+octet >> 4  -> desplaza los 4 bits más significativos que son los primeros 4 bits de octet hacia la derecha, 
+esto deja a los 4 bits menos significativos como cero
+| es el operador OR que combina los resultados                 
+octet << 4 -> desplaza los 4 bits menos significativos del byte octet hacia la izquierda 
+  y coloca los 4 inferiores (más a la izda.)
+*/
