fee28a0d9b9c6b24878e37c98c9b040c66da6bda
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-03 21:12:23 +0200

    reverse_bits

--- nivel2/reverse_bits.c
+++ nivel2/reverse_bits.c
@@ -34,3 +34,19 @@ unsigned char	reverse_bits(unsigned char octet)
     return (byte)
     
 }
+
+/*
+Esta función toma un número de 8 bits que en este caso es octet e invierte el orden de sus bits.
+-Declaramos dos variables
+Un unsigned char byte = 0y un unsigned int i = 8
+-Recorre i hacia atrás 
+    (Cada vez que el bucle se ejecuta, i se decrementa, y el bucle se repite 8 veces (para cada bit del octeto)).
+byte = (byte << 1) | (octet & 1)
+
+Desplaza los bits de byte una posición a la izquierda (byte << 1), preparando espacio para el siguiente bit.
+(octet & 1) obtiene el bit menos significativo de octet (el bit más a la derecha).
+Luego, con | (OR bit a bit), coloca este bit en el espacio más a la izquierda de byte.
+octet = octet >> 1;:
+Desplaza los bits de octet una posición a la derecha para procesar el siguiente bit en la siguiente iteración.
+Retornamos byte que tiene los bits de octet invertidos
+*/
