e913cce9b41b9c45ad100d6ff9e8af8a1afbaefc
Raquel Cabrero <cabrero.raquel@gmail.com>
2025-10-25 22:31:57 +0200

    add_prime_sum

--- nivel3/add_prime_sum.c
+++ nivel3/add_prime_sum.c
@@ -27,7 +27,7 @@ $>
 
 int ft_isspace(char c)
 {
-    return (c == ' '|| c == '\t');
+    return (c == ' ' || c == '\t');
 }
 
 int ft_atoi(char *str)
@@ -46,10 +46,10 @@ int ft_atoi(char *str)
     }
     while(str[i] >= '0' && str[i] <= '9')
     {
-        result = result * 10 + (str[i] - 48);
+        result = (result * 10) + (str[i] - 48);
         i++;
     }
-    return (sign * result);
+    return (result * sign);
 }
 
 void ft_putnbr(int nb)
@@ -62,35 +62,32 @@ void ft_putnbr(int nb)
     write(1, &c, 1);
 }
 
-int is_prime(int n)
+int is_prime (int nb) // nb = 5
 {
     int i = 2;
-
-    if (n <= 1)
+    
+    if (nb <= 1)
         return (0);
-    while (i * i <= n)
+    while(i * i <= nb)
     {
-        if (n % i == 0)
+        if (nb % i == 0)
             return (0);
         i++;
     }
-    return(1);
+    return (1);
 }
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-    int num; 
-    int i = 2;
-    int sum = 0;
-
+   int i = 2;                           //argv = 7
+   int num;
+   int sum;
+                            
     if (argc == 2)
     {
         num = ft_atoi(argv[1]);
-        if (num <= 0)
-        {
-            write(1, "0", 1);
-            return (0);
-        }
+        if (i <= 0)
+            write(1, "0\n", 2);
         while(i <= num)
         {
             if (is_prime(i))
@@ -98,48 +95,8 @@ int main(int argc, char **argv)
             i++;
         }
         ft_putnbr(sum);
-    write(1, "\n", 1);        
+        write(1, "\n", 1);
     }
-    else
-    {
+    else 
     write(1, "0\n", 2);
-    return (0);
-    }
 }
-
-/*
-Necesitamos crear 4 funciones auxiliares (en realidad pueden ser solo 3 si hacemos un atoi simplificado)
-ft_isspace -> para gestionar los espacios de ft_atoi
-ft_atoi -> para convertir el argumento de la función principal en un número
-is_prime -> la utilizamos para saber si un número es primo o no.
-
-is_prime:
-Esta función verifica si un número n es primo:
-    - descartamos los números menores o iguales a 1 (no son primos).
-    - Luego verifica si hay algún divisor i desde 2 hasta la raíz cuadrada de n (i * i <= n).
-        - Si encuentra un divisor (n % i == 0), 
-            - devuelve 0 (no es primo).
-        - i++
-    - Si no encuentra divisores, devuelve 1 (es primo).
-
-Función principal:
-
-Declaramos tres variables:
-int num -> el número que le vamos a pasar 
-int i = 2 -> empieza en 2 porque es el primer número primo
-int sum = 0 -> usamos esto para el resultado de la suma
-
-- Comprobamos el número de argumentos
-    - convertimos el argumento con atoi -> num
-    - Si el número (num) es menor o igual a cero
-        - imprime cero y sale
-    - Mientras i (2) es menor o igual que num 
-        - Si es primo (i)
-            - le suma i a sum
-        i++
-    -escribe num con ft_putnbr
-- escribe salto de línea
-else
-    Escribe cero y salto de línea
-    devuelve cero
-*/
